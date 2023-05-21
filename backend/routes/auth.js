f9ecdd2c8e73f731092e52823740bf6a2bebf18f
akshayjain07 <akshay.070603@gmail.com>
2023-05-21 18:32:00 +0530

    lec49

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,30 +1,11 @@
-// const express = require('express');
-// const router = express.Router();
-// const { body, validationResult } = require('express-validator');
-// const User = require('../models/User')
-
-
-// router.post('/',[
-//     body('name').notEmpty().withMessage('Name is required'),
-//     body('email').isEmail().withMessage('Invalid email address'),
-//     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters')
-//   ], (req,res)=>{
-//         const errors = validationResult(req);
-//         if (!errors.isEmpty()) {
-//             return res.status(400).json({ errors: errors.array() });
-//         }
-//         console.log(req.body);
-//         const user = User(req.body);
-//         user.save();
-//         res.send(req.body);
-// })
-// module.exports = router
-
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const User = require('../models/User')
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
+const JWT_SECRET = 'harryisagoodb$oy';
 
 router.post('/createuser',[
     body('name','Name is required').notEmpty(),
@@ -42,12 +23,24 @@ router.post('/createuser',[
             if(existingUser){
                 return res.status(400).json({error:"This a user with this email already exist"})
             }
+
+            const salt = await bcrypt.genSaltSync(10);
+            const secPass = await bcrypt.hash(req.body.password,salt);
+
             const user = await User.create({
                 name:req.body.name,
                 email:req.body.email,
-                password:req.body.password
+                password:secPass
             })
-            res.json(user)
+            const data = {
+                user:{
+                    id:user.id
+                }
+            }
+            const token = jwt.sign(data, 'shhhhh');
+            // console.log(token) 
+
+            res.json({token})
         }catch(error){
             console.error(error.message);
             res.status(500).send("Some error occured")
