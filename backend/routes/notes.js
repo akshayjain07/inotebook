057067f8c4374bcae7ae3504c7bce3c40b4d6c20
akshayjain07 <akshay.070603@gmail.com>
2023-05-23 17:50:43 +0530

    lec53

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -17,14 +17,14 @@ router.get('/fetchallnotes', fetchuser, async (req,res)=>{
 })
 
 // Route 2: Add a new Note using: POST "/api/notes/addnote". Login required
-router.post('/fetchallnotes', fetchuser, [
+router.post('/addnote', fetchuser, [
     body('title','Title must be atleast 3 characters').isLength({ min: 3 }),
     body('description','Description must be atleast 5 characters').isLength({ min: 5 }),
    ],async (req,res)=>{
     
-    try {
-        const {title, description, tag} = req.body;
+       const {title, description, tag} = req.body;
     
+       try {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
@@ -40,6 +40,33 @@ router.post('/fetchallnotes', fetchuser, [
         console.error(error.message);
         res.status(500).send("Internal server error");
     }
+})
+
+//Route 3: Update an existing note using: PUT "/api/notes/updatenote". Login required
+router.put('/updatenote/:id', fetchuser, async (req,res)=>{
+    const {title, description, tag} = req.body;
+    // create a newNote object
+    const newNote = {};
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+
+    // Find the note to be updated and update it
+    let note = await Note.findById(req.params.id);    // this id is /:id ye wali id
+
+    // If the note does not exist 
+    if(!note){return res.status(404).send("not found")}
+
+    // check the user is right or hacker
+    // 1) will give is note ki id 2) request me jo id maang rha hai 
+    if(note.user.toString() !== req.user.id){               
+        return res.status(401).send("not allowed")
+    }
+
+    // now finally updating it 
+    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+    res.json(note);
+
 })
 
 module.exports = router
