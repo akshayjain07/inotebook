a69f47039c57be804655e4ea141f17104d1f71f6
akshayjain07 <akshay.070603@gmail.com>
2023-05-24 11:37:26 +0530

    lec57

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
+// import './App.css';
+// import About from './components/About';
+// import Home from './components/Home';
+// // import Navbar from './components/Navbar';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+//   // Link
+// } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <>
+//         <Router>
+//           <Navbar/>
+//           <Routes>
+//               <Route exact path="/" element={<Home/>} />
+//               <Route exact path="/about" element={<About/>} />
+//             </Routes>
+//           </Router>
+//       </>
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>This is my react app </h1>
+      <>
+        <Router>
+          <Navbar/>
+          <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route exact path="/about" element={<About/>} />
+          </Routes>
+        </Router>
+      </>
+
     </div>
   );
 }
